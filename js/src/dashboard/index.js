3bf49c378d00219be0c51ec171f9395c7608157f
totomasto <to.tomas@yahoo.com>
2019-06-18 22:43:51 +0300

    Reports 2

--- js/src/dashboard/index.js
+++ js/src/dashboard/index.js
@@ -21,7 +21,6 @@ let displayStats = async () => {
 
 }
 
-displayStats();
 
 window.setInterval(async ()=>{
 
@@ -39,8 +38,44 @@ window.setInterval(async ()=>{
 }, 5000);
 
 
+let displayLeadDataByMonth = async () => {
+
+let leads = JSON.parse(localStorage.getItem('leadsData'));
+
+leads.forEach(lead => {
+    
+  // Split timestamp into [ Y, M, D, h, m, s ] and retrieving t[1] thats the month to map the chart
+  //t is the month in 01 format so it needs conversion to jan, feb etc. and also counting the leads from each date 
+  
+  let t = lead.date.split(/[- :]/);
+  console.log(t[1]);
+
+  
+
+});
+
+
+new Chart(document.getElementById("chart2"),
+{"type":"bar",
+"data":{"labels":["January","February","March","April","May","June","July"]
+,"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"backgroundColor":["rgba(236, 94, 105, 0.2)","rgba(255, 159, 64, 0.2)","rgba(241, 194, 5, 0.2)","rgba(99, 203, 137, 0.2)","rgba(0, 112, 224, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],"borderColor":["rgb(236, 94, 105)","rgb(255, 159, 64)","rgb(241, 194, 5)","rgb(99, 203, 137)","rgb(0, 112, 224)","rgb(153, 102, 255)","rgb(201, 203, 207)"],"borderWidth":1}]}
+,"options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}});
 
 
 
-new Chart(document.getElementById("chart2"),{"type":"bar","data":{"labels":["January","February","March","April","May","June","July"],"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"backgroundColor":["rgba(236, 94, 105, 0.2)","rgba(255, 159, 64, 0.2)","rgba(241, 194, 5, 0.2)","rgba(99, 203, 137, 0.2)","rgba(0, 112, 224, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],"borderColor":["rgb(236, 94, 105)","rgb(255, 159, 64)","rgb(241, 194, 5)","rgb(99, 203, 137)","rgb(0, 112, 224)","rgb(153, 102, 255)","rgb(201, 203, 207)"],"borderWidth":1}]},"options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}});
-new Chart(document.getElementById("chart4"),{"type":"doughnut","data":{"labels":["Red","Blue","Yellow"],"datasets":[{"label":"My First Dataset","data":[300,50,100],"backgroundColor":["rgb(236, 94, 105)","rgb(0, 112, 224)","rgb(241, 194, 5)"]}]}});
+}
+
+
+displayLeadDataByMonth();
+
+
+
+new Chart(document.getElementById("chart4"),
+{"type":"doughnut","data":{"labels":["Red","Blue","Yellow"],
+"datasets":[{"label":"My First Dataset","data":[300,50,100]
+,"backgroundColor":["rgb(236, 94, 105)","rgb(0, 112, 224)","rgb(241, 194, 5)"]}]}});
+
+
+
+
+
